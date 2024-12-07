1a69db1fc4be2d97fa0dbb1f6dbab9221b1482c2
devhassan19 <devhassan19@email.com>
2024-12-07 18:40:49 +0300

    frs submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import Weather from './components/WeatherForecast'
 
 const App = () => {
-  return <h1>Hello World !!!!!!!!!!!!!!!</h1>
+  return (
+    <>
+      <h1>Local Weather</h1>
+      <section></section>
+    </>
+  )
 }
 
 export default App
